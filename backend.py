3e3908a74ced545a36b5243fa97c9d2887a5cafc
Semicide <190101033@ogr.atu.edu.tr>
2024-05-25 16:59:03 +0300

    New predictions
    Fixes and new models to predict.

--- backend.py
+++ backend.py
@@ -1,14 +1,18 @@
+import joblib
 from flask import Flask, render_template, request, redirect, url_for
 from keras.models import load_model
 import numpy as np
 from flask_sqlalchemy import SQLAlchemy
 import datetime
 import haversine as hs
-#DB connection
+
+# DB connection
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres.olwlkyaowkgdrbonvxpz:KdHg2RbLxjxhRlL7@aws-0-eu-central-1.pooler.supabase.com:5432/postgres'
+app.config[
+    'SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres.olwlkyaowkgdrbonvxpz:KdHg2RbLxjxhRlL7@aws-0-eu-central-1.pooler.supabase.com:5432/postgres'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
 
 # Define database models
 class Client(db.Model):
@@ -17,6 +21,7 @@ class Client(db.Model):
     homelocationX = db.Column(db.Float)
     homelocationY = db.Column(db.Float)
 
+
 class Transaction(db.Model):
     tr_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     tr_date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
@@ -24,17 +29,29 @@ class Transaction(db.Model):
     retailer = db.Column(db.Integer, nullable=False)
     used_chip = db.Column(db.Integer)
     used_pin = db.Column(db.Integer)
-    tr_price = db.Column(db.Float,nullable=False)
+    tr_price = db.Column(db.Float, nullable=False)
     online_order = db.Column(db.Integer)
     transactionlocationX = db.Column(db.Float)
     transactionlocationY = db.Column(db.Float)
+
 
 # Create database tables within Flask application context
 with app.app_context():
     db.create_all()
 
-# Modeli yükle
-model = load_model("balanced_model.keras")
+# Load scaling parameters
+with open('scaling_params.txt', 'r') as f:
+    scaling_params = {}
+    for line in f:
+        column, min_val, max_val = line.strip().split(',')
+        scaling_params[column] = {'min': float(min_val), 'max': float(max_val)}
+
+# Load models
+KNN_model = joblib.load('KNN_best_model.pkl')
+log_model = joblib.load('Logistic Regression_best_model.pkl')
+NB_model = joblib.load('Naive Bayes_best_model.pkl')
+ann_model = load_model("fraud_detection_model.h5")
+
 
 @app.route('/')
 def index():
@@ -42,11 +59,11 @@ def index():
     transactions = Transaction.query.all()
     return render_template('index.html', clients=clients, transactions=transactions)
 
+
 @app.route('/predict', methods=['GET'])
 def predict():
     client_id = request.form.get('client_id')
     transaction_id = request.form.get('transaction_id')
-    # Tahmin sonucunu döndür ve index.html'e gönder
     return render_template('index.html')
 
 
@@ -74,7 +91,7 @@ def add_transaction():
         used_chip = request.form['used_chip']
         used_pin = request.form['used_pin']
         online_order = request.form['online_order']
-        tr_price=request.form['tr_price']
+        tr_price = request.form['tr_price']
         transactionlocationX = request.form['transactionlocationX']
         transactionlocationY = request.form['transactionlocationY']
 
@@ -85,13 +102,15 @@ def add_transaction():
         db.session.commit()
 
         return redirect(url_for('index'))
-    clients = Client.query.all()  # Fetch all clients
+    clients = Client.query.all()
     return render_template('transaction_form.html', clients=clients)
+
 
 @app.route('/clients')
 def clients():
     clients = Client.query.all()
     return render_template('clients.html', clients=clients)
+
 
 @app.route('/transactions/<int:client_id>')
 def transactions(client_id):
@@ -99,79 +118,87 @@ def transactions(client_id):
     transactions = Transaction.query.filter_by(cl_id=client_id).all()
     return render_template('transactions.html', client=client, transactions=transactions)
 
-@app.route('/transaction_perdict/<int:client_id>/<int:transaction_id>', methods=['GET', 'POST'])
-def predict_transaction(transaction_id,client_id):
-    # Fetch the selected client and transaction
+
+@app.route('/transaction_predict/<int:client_id>/<int:transaction_id>', methods=['GET', 'POST'])
+def predict_transaction(client_id, transaction_id):
     client = Client.query.get(client_id)
     transaction = Transaction.query.get(transaction_id)
 
     if client and transaction:
-        # Calculate distance from home using Haversine formula
         distance_from_home = hs.haversine((client.homelocationX, client.homelocationY),
                                           (transaction.transactionlocationX, transaction.transactionlocationY))
 
-        # Fetch previous transactions for the client
         previous_transactions = Transaction.query.filter_by(cl_id=client_id).order_by(Transaction.tr_date.desc()).all()
 
-        # Calculate distance from last transaction if there are previous transactions
-        if len(previous_transactions)>1:
+        if len(previous_transactions) > 1:
             last_transaction = previous_transactions[1]
-            distance_from_last_transaction = hs.haversine((last_transaction.transactionlocationX, last_transaction.transactionlocationY),
-                                                           (transaction.transactionlocationX, transaction.transactionlocationY))
+            distance_from_last_transaction = hs.haversine(
+                (last_transaction.transactionlocationX, last_transaction.transactionlocationY),
+                (transaction.transactionlocationX, transaction.transactionlocationY))
         else:
-            # If no previous transactions, set distance_from_last_transaction to 0
             distance_from_last_transaction = 0
 
-        # Calculate median purchase price
-        all_transaction_prices = [prev_transaction.tr_price for prev_transaction in previous_transactions]
+        all_transaction_prices = [t.tr_price for t in previous_transactions]
         median_purchase_price = np.median(all_transaction_prices) if all_transaction_prices else 0
 
-        # Calculate ratio to median purchase price
         ratio_to_median_purchase_price = transaction.tr_price / median_purchase_price if median_purchase_price != 0 else 0
-        retailers = [prev_transaction.retailer for prev_transaction in previous_transactions]
-        if transaction.retailer in retailers:
-            repeat_purchase = 1
-            if retailers.count(transaction.retailer) == 1:
-                repeat_purchase = 0
-        else:
-            repeat_purchase = 0
-        # Extract features from client and transaction data
-        feature1 = distance_from_home
-        feature2 = distance_from_last_transaction
-        feature3 = ratio_to_median_purchase_price
-        feature4 = repeat_purchase
-        feature5 = transaction.used_chip
-        feature6 = transaction.used_pin
-        feature7 = transaction.online_order
-        features1 = {
+
+        retailers = [t.retailer for t in previous_transactions]
+        repeat_purchase = 1 if transaction.retailer in retailers else 0
+
+        features = np.array([[
+            distance_from_home,
+            distance_from_last_transaction,
+            ratio_to_median_purchase_price,
+            repeat_purchase,
+            transaction.used_chip,
+            transaction.used_pin,
+            transaction.online_order
+        ]])
+
+        features_dict = {
             "Distance from home": distance_from_home,
             "Distance from last transaction": distance_from_last_transaction,
             "Ratio to median purchase price": ratio_to_median_purchase_price,
-            "repeat_purchase": repeat_purchase,
+            "Repeat purchase": repeat_purchase,
             "Used chip": transaction.used_chip,
             "Used pin": transaction.used_pin,
-            "Online order": transaction.online_order,
-            "median_purchase_price":median_purchase_price,
-
+            "Online order": transaction.online_order
         }
 
-        # Scale the features
-        features = np.array([[feature1, feature2, feature3, feature4, feature5, feature6, feature7]])
+        # Normalize the features
+        features[:, 0] = (features[:, 0] - scaling_params['distance_from_home']['min']) / (
+                    scaling_params['distance_from_home']['max'] - scaling_params['distance_from_home']['min'])
+        features[:, 1] = (features[:, 1] - scaling_params['distance_from_last_transaction']['min']) / (
+                    scaling_params['distance_from_last_transaction']['max'] -
+                    scaling_params['distance_from_last_transaction']['min'])
+        features[:, 2] = (features[:, 2] - scaling_params['ratio_to_median_purchase_price']['min']) / (
+                    scaling_params['ratio_to_median_purchase_price']['max'] -
+                    scaling_params['ratio_to_median_purchase_price']['min'])
+        features[:, 3] = (features[:, 3] - scaling_params['repeat_retailer']['min']) / (
+                    scaling_params['repeat_retailer']['max'] - scaling_params['repeat_retailer']['min'])
+        features[:, 4] = (features[:, 4] - scaling_params['used_chip']['min']) / (
+                    scaling_params['used_chip']['max'] - scaling_params['used_chip']['min'])
+        features[:, 5] = (features[:, 5] - scaling_params['used_pin_number']['min']) / (
+                    scaling_params['used_pin_number']['max'] - scaling_params['used_pin_number']['min'])
+        features[:, 6] = (features[:, 6] - scaling_params['online_order']['min']) / (
+                    scaling_params['online_order']['max'] - scaling_params['online_order']['min'])
 
-        # Make prediction
-        prediction = model.predict(features)
+        KNN_prediction = KNN_model.predict(features)[0]
+        log_prediction = log_model.predict(features)[0]
+        NB_prediction = NB_model.predict(features)[0]
+        ann_prediction = ann_model.predict(features)[0][0]
 
-        if prediction >= 0.5:
-            prediction_result = "Fraud"
-        else:
-            prediction_result = "Not Fraud"
+        predictions = {
+            'KNN Prediction': 'Fraud' if KNN_prediction == 1 else 'Not Fraud',
+            'Logistic Regression Prediction': 'Fraud' if log_prediction == 1 else 'Not Fraud',
+            'Naive Bayes Prediction': 'Fraud' if NB_prediction == 1 else 'Not Fraud',
+            'ANN Prediction': 'Fraud' if ann_prediction >= 0.5 else 'Not Fraud'
+        }
 
-        return render_template('predict_transaction.html', prediction=prediction_result, clients=Client.query.all(),
-                               transactions=Transaction.query.all(),features=features1)
-    else:
-        # Handle error when client or transaction is not found
-        prediction_result = "Client or transaction not found"
+        return render_template('predict_transaction.html', client=client, transaction=transaction,
+                               predictions=predictions, features=features_dict)
 
-    return render_template('clients.html', prediction=prediction_result)
+
 if __name__ == '__main__':
     app.run(debug=True)
